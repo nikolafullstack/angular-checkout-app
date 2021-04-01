fca2b3055bb9a0133b60d26417cfc9a0fe8c14ff
nikolafullstack <nikola.todor701@gmail.com>
2021-04-01 13:26:19 +0200

    update app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-test-app'`, () => {
+  it(`should have as title 'angular-checkout-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-test-app');
+    expect(app.title).toEqual('angular-checkout-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-test-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-checkout-app app is running!');
   });
 });
